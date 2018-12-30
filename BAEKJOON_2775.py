976e626cd7f90e72b4b6c785bda85c74c4c714d5
arkss <rkdalstjd9@naver.com>
2018-12-31 07:20:57 +0900

    BAEKJOON_2775.py 부녀회장이 될테야

--- BAEKJOON_2775.py
+++ BAEKJOON_2775.py
@@ -16,3 +16,18 @@ for i in range(case):
                 apart[j+1][i-1] += apart[j][k]
     
     print(apart[floor][number-1])
+
+    """
+    배열을 굳이 2차원 배열로 할 필요가 없었다..
+    t=int(input())
+for i in range(t):
+    k=int(input())
+    n=int(input())
+    arr=[j for j in range(1,n+1)]
+    for l in range(k):
+        for j in range(n-1):
+            arr[j+1]+=arr[j]
+        #print(arr)
+    print(arr)
+    print(arr[-1])
+    """
