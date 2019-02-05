2d427f89316b6b87bb79d30189fdcfcd10c76a79
minsung <rkdalstjd9@naver.com>
2019-02-06 02:00:11 +0900

    연속합

--- BAEKJOON_1912/BAEKJOON_1912/main.cpp
+++ BAEKJOON_1912/BAEKJOON_1912/main.cpp
@@ -6,48 +6,34 @@ int max(int a, int b)
 }
 int main(int argc, const char * argv[]) {
     int num; // 몇 개의 숫자가 들어오는지 결정해주는 변수
-    int i,j;
+    int i;
     int result = 0; // 최종적인 최대값을 저장할 변수
-    int temp = 0;
    
     
     std::cin >> num;
     
-    int arr[num]; // 입력 수들을 저장할 배열
-    int dp[num]; // 각 index 마다 해당 index에서 시작했을 때의 최대값을 저장
-    
-    // temp[num] 0으로 초기화.
-    for (i=0;i<num;i++){
-        dp[i] = 0;
-    }
+    int arr[num+1]; // 입력 수들을 저장할 배열
+    int dp[num+1]; // 각 index 마다 해당 index까지의 최대값을 저장
     
     // 값입력
     for (i=0;i<num;i++){
         std::cin >> arr[i];
     }
     
-//
-//    dp[0] = max(dp[0], arr[0]);
-//    dp[0] = max(dp[0], arr[0] + arr[1]);
-//    dp[0] = max(dp[0], arr[0]+arr[1]+arr[2]);
     
+    // 전 항까지의 최대값과 현재 항의 값을 비교하며 최대값을 취한다.
+    dp[0] = arr[0];
     for (i=0;i<num;i++){
-        temp = 0;
-        for (j=i;j<num;j++){
-            temp += arr[j];
-            dp[i] = max(dp[i],temp);
-        }
+        dp[i+1] = max(dp[i]+arr[i+1],arr[i+1]);
     }
     
-    for (i=0;i<num;i++){
-        if (dp[i] > result)
+    // dp 배열은 각 index까지의 최대값이 담겨있으므로 이를 비교해서 최대값을 result에 저장
+    result = dp[0];
+    for (i=1;i<num;i++){
+        if (result < dp[i])
             result = dp[i];
     }
 
+    // 값 출력
     std::cout << result << std::endl;
-    
-//    for (i=0;i<num;i++){
-//        std::cout << dp[i] << std::endl;
-//    }
-    
 }
