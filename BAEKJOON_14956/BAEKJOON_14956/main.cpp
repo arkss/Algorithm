4726ec05e9b0f7416c530dd575ac584402d65edf
minsung <rkdalstjd9@naver.com>
2019-07-21 05:21:05 +0900

    acmicpc 2017

--- BAEKJOON_14956/BAEKJOON_14956/main.cpp
+++ BAEKJOON_14956/BAEKJOON_14956/main.cpp
@@ -1,36 +1,78 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-
+pair<int, int> hilbert(int N, int M)
+{
+    pair<int, int> p;
     
-    return 0;
+    // W1 일 때
+    if (N == 2)
+    {
+        switch (M)
+        {
+            case 0:
+                p = { 1, 1 };
+                return p;
+            case 1:
+                p = { 1, 2 };
+                return p;
+            case 2:
+                p = { 2, 2 };
+                return p;
+            case 3:
+                p = { 2, 1 };
+                return p;
+        }
+    }
+    // Wn-1 에 해당
+    int half = N / 2;
+    // Wn 에 대해 4구역으로 나누면 한 구역 half*half 개의 이동이 필요하다. 즉 M / (half*half) 을 함으로서 4개 구역 중 어디에 있을 지를 고른다.
+    int quadrant = M / (half*half);
+    
+    switch (quadrant)
+    {
+            //좌하단
+        case 0:
+            p = hilbert(half, M % (half*half));
+            // first와 second를 바꾸는건 y=x 대칭
+            swap(p.first, p.second);
+            return p;
+            
+            //좌상단
+        case 1:
+            p = hilbert(half, M % (half*half));
+            // 위로 평행이동
+            p.second += half;
+            return p;
+            
+            //우상단
+        case 2:
+            p = hilbert(half, M % (half*half));
+            // 대각선으로 평행이동
+            p.first += half;
+            p.second += half;
+            return p;
+            
+            //우하단
+        case 3:
+            p = hilbert(half, M % (half*half));
+            pair<int, int> temp = { 2 * half - p.second + 1, half - p.first + 1 };
+            return temp;
+    }
+    return p;
 }
 
 
-/*
- 
- 전체적인 구조가 비슷
- 
- 2^k 정사각형
- 
- k=1)
- 1미터 갈때마다 우회전
- 
- k>1)
- k-1 일 때의 것 4개로 구성
- 왼쪽아래를 선택할 때, k-1 전체를 오른쪽으로 90 회전
- 
- 철학자들은 자신이 얼만큼 걸었는지 알고 있음.
- 또한 k 값도 알고 있음.
- 
- 철학자의 위치를 좌표로 계산해야한다.
- 왼쪽 하단 모서리 블록 좌표가 (1,1)
- 철학자는 항상 (1,1) 에서 시작, 출구는 (n,1)
- 
- n : 미로의 한 변의 길이
- m : step 수
- 
- 왜 예시가 내가 생각하는거보다 1칸 씩 전에 있지?
- */
+
+int main(void)
+{
+    int N, M;
+    cin >> N >> M;
+    //0부터
+    pair<int ,int> result = hilbert(N, M - 1);
+    cout << result.first << " " << result.second << "\n";
+    
+    return 0;
+}
