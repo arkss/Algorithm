ffbedadd6427e96071d4c8ffc62b465eeb91c42a
minsung <rkdalstjd9@naver.com>
2019-07-30 22:28:59 +0900

    acmicpc 2012

---
+++ BAEKJOON_8895/BAEKJOON_8895/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BAEKJOON_8895
+//
+//  Created by ark on 29/07/2019.
+//  Copyright © 2019 ark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- BAEKJOON_9466/BAEKJOON_9466/main.cpp
+++ BAEKJOON_9466/BAEKJOON_9466/main.cpp
@@ -4,36 +4,47 @@ using namespace std;
 
 // 값을 받을 리스트
 int list[100001];
-int checkList[100001];
-int circledList[100001];
+// 0: 방문 x, 1: 현재 순회중, 2: 방문 완료
+int visited[100001];
+// 사이클이 만들어진 애들을 체크해주자.
+int circled[100001];
 
-int N;
 
 int flag = 0;
 int startIndex = 0;
 
+// 전체 사람의 수, team 을 이룬 사람의 수
+// 두 값의 차이는 team 을 이루지 못한 사람의 수
+int N;
 int teamNum = 0;
 
 void dfs(int i){
-    // 맨 처음에 dfs 를 들어왔을 때만 첫번째 값을 저장
     if (flag){
         startIndex = i;
         flag = 0;
-        
     }
-    checkList[i] = 1;
-    // 이번 사이클에 있는 index 표시
-    circledList[i] = 1;
-    int nextIndex = list[i];
-    if (startIndex==nextIndex){
-            for (int j=1;j<=N;j++){
-                // 팀원의 수만큼 count
-                if (circledList[j]) teamNum++;
-            }
+    // 이미 사이클을 만든 노드는 순회할 필요가 없다.
+    if (circled[i]) return;
+    // 이미 끝난 상황이면 순회할 필요가 없다.
+    if (visited[i] == -1) return;
+    
+    // 방문을 안했으면
+    if (!visited[i]){
+        // 현재 순회중
+        visited[i] = startIndex;
     }
-    if (!checkList[nextIndex])
-        dfs(nextIndex);
+    // 현재 순회중
+    else if (visited[i] == startIndex){
+        circled[i] = 1;
+        teamNum++;
+    }
+    int nextNode = list[i];
+    dfs(nextNode);
+    
+    visited[i] = -1;
 }
+
+
 
 int main(int argc, const char * argv[]) {
     
@@ -43,32 +54,46 @@ int main(int argc, const char * argv[]) {
     for (int t=0;t<T;t++){
         
         cin >> N;
+        teamNum = 0;
         
         for (int i=1;i<=N;i++){
             cin >> list[i];
         }
-
+        
         for (int i=1;i<=N;i++){
-            
-            // circledList 초기화
-            for (int j=1;j<=N;j++){
-                circledList[j] = 0;
+            if (!visited[i]){
+                flag = 1;
+                dfs(i);
+                }
+                
             }
-            flag = 1;
-            dfs(i);
-        }
-
+        
+//        for (int i=1;i<=N;i++){
+//            cout << circled[i] << " " ;
+//        }
+//        cout << endl;
+        
         cout << N-teamNum << endl;
         
         // 기본값 초기화
         for (int i=1;i<=N;i++){
-            checkList[i] = 0;
+            list[i] = 0;
+            visited[i] = 0;
+            circled[i] = 0;
         }
         teamNum = 0;
-    }
+        }
     return 0;
 }
 
-
-
-
+/*
+ 
+ visited에 들어온 수를 넣어준다.
+ 
+ 순회하면서 visited 가 들어온 수와 같으면 cycle 이 형성된 것.
+ 
+ dfs가 끝나는 순간에 visited에 -1 을 넣고 더 이상 순회하지 않도록 한다.
+ 이미 사이클이 만들어졌어도 순회하지 않도록 한다.
+ 
+ 
+ */
