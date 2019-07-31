f3aa372275e9ca3120ba570c2db572978bfbb8b7
minsung <rkdalstjd9@naver.com>
2019-07-31 17:02:12 +0900

    막대배치

--- BAEKJOON_8895/BAEKJOON_8895/main.cpp
+++ BAEKJOON_8895/BAEKJOON_8895/main.cpp
@@ -2,33 +2,25 @@
 
 using namespace std;
 
+long long dp[21][21][21];
+
 int main(int argc, const char * argv[]) {
     
+    dp[1][1][1] = 1;
+    for (int n=2;n<=20;n++){
+        for (int l=1;l<=20;l++){
+            for (int r=1;r<=20;r++){
+                dp[n][l][r] = dp[n-1][l-1][r] + dp[n-1][l][r-1] + (n-2)*dp[n-1][l][r];
+            }
+        }
+    }
     
+    int T;
+    cin >> T;
+    for (int t=0;t<T;t++){
+        int n,l,r;
+        cin >> n >> l >> r;
+        cout << dp[n][l][r] << endl;
+    }
     return 0;
 }
-
-/*
- 
- 길이가 1~n 인 n 개의 막대가 있다.
- 
- 왼쪽이나 오른쪽에서 보면 작은 막대는 가려져서 안보인다.
- 
- 그림에서는 왼쪽에서는 1개의 막대가, 오른쪽에서는 2개의 막대가 보인다.
- 
- 
-왼쪽에서는 l 개의 막대가, 오른쪽에서는 r 개의 막대가 보이는 막대 배열의 수를 구해라
- 
- [ 입력 ]
- 
- T 개의 test case
- 
- 막대의 개수는 n
- 왼쪽에서는 l 개의 막대가, 오른쪽에서는 r 개의 막대가 보인다.
- 
- n, l, and r (1 ≤ l,r ≤ n ≤ 20)
- 
- 
- 
- 
- */
