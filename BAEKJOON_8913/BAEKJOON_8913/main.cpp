038f4fa3b396548fd5f60e5a359170e027695c78
minsung <rkdalstjd9@naver.com>
2019-08-04 03:04:03 +0900

    실패

---
+++ BAEKJOON_8913/BAEKJOON_8913/main.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    string str;
+    cin >> str;
+    
+    int check[25] = {0};
+    
+    //
+    int aPoint , bPoint;
+    if (str[0] == 'a') aPoint = 0;
+    else bPoint = 0;
+    
+    for (int i=1;i<str.length();i++){
+        
+        
+    }
+    
+    
+    
+    
+    
+}
+
+/*
+ 
+ dp
+ 
+ 문자열 길이 만큼 dp
+ 
+ 
+ */
+
