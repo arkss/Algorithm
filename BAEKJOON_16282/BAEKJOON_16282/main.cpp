c9c32494d17bb2ef8c92131805590a8f97dd51da
minsung <rkdalstjd9@naver.com>
2019-09-29 19:43:48 +0900

    [acmicpc]Black Chain

--- BAEKJOON_16282/BAEKJOON_16282/main.cpp
+++ BAEKJOON_16282/BAEKJOON_16282/main.cpp
@@ -7,11 +7,8 @@ int main(int argc, const char * argv[]) {
     long long n;
     cin >> n;
     
-    long long powerOfTwo = 2;
     for (long long i=1;i<=n;i++){
-        powerOfTwo *= 2;
-        long long limit = (i+1)*(powerOfTwo-1) + i;
-
+        long long limit = (i+1LL)*((1LL<<(i+1))-1) + i;
         if (n<=limit){
             cout << i;
             break;
