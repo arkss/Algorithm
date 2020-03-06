9abf9164cebdfbc0ea9b5f7d714e843403cc17ef
minsung <rkdalstjd9@naver.com>
2020-03-06 16:51:36 +0900

    [level1]기능개발

---
+++ programmers/.vscode/기능개발.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds){
+    vector<int> days;
+    vector<int> answer;
+    int size = speeds.size();
+    for (int i=0;i<size;i++){
+        int day = (100 - progresses[i]) / speeds[i];
+        days.push_back(day);
+    }
+ //7 4 9
+    int cnt = 0;
+    int prev_day;
+    for (int i=0;i<size;i++){
+        if (cnt == 0){
+            prev_day = days[i];
+            cnt++;
+        }
+        else if  (prev_day >= days[i]) cnt++;
+        else {
+            answer.push_back(cnt);
+            prev_day = days[i];
+            cnt = 1;
+        }
+    }
+    answer.push_back(cnt);
+    return answer;
+
+    // for (int i=0;i<answer.size();i++){
+    //     cout << answer[i] << "\n";
+    // }
+}
+
+int main(){
+    vector<int> progresses;
+    vector<int> speeds;
+
+    progresses.push_back(93);
+    progresses.push_back(30);
+    progresses.push_back(55);
+    speeds.push_back(1);
+    speeds.push_back(30);
+    speeds.push_back(5);
+    solution(progresses, speeds);
+}
