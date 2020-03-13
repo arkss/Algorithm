6d201581d21f23de02c59f955a4ab483ed4d7093
minsung <rkdalstjd9@naver.com>
2020-03-13 17:56:25 +0900

    [level2]행렬의 곱셈

---
+++ programmers/.vscode/행렬의곱셉.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
+{
+    vector<vector<int>> answer;
+    for (int i = 0; i < arr1.size(); i++)
+    {
+        vector<int> temp;
+        for (int j = 0; j < arr2[0].size(); j++)
+        {
+            int num = 0;
+            for (int k = 0; k < arr1[0].size(); k++)
+            {
+                num += arr1[i][k] * arr2[k][j];
+            }
+            temp.push_back(num);
+        }
+        answer.push_back(temp);
+    }
+    return answer;
+}
+
+int main()
+{
+    return 0;
+}
