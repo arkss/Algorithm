8617dd13f9f1484ec50e1ae7007f97eade24cb4a
minsung <rkdalstjd9@naver.com>
2020-04-07 17:06:27 +0900

    [level3]등굣길

--- programmers/level3/등굣길.cpp
+++ programmers/level3/등굣길.cpp
@@ -7,14 +7,13 @@ int dp[103][103];
 
 int solution(int m,int n, vector<vector<int>> puddles){
     for (int i=0;i<puddles.size();i++){
-        int x = puddles[i][0];
-        int y = puddles[i][1];
+        int x = puddles[i][1];
+        int y = puddles[i][0];
         dp[x][y] = -1;
     }
-    dp[1][1] = 1;
+    dp[1][0] = 1;
     for (int i=1;i<=n;i++){
         for (int j=1;j<=m;j++){
-            if (i== 1 && j==1) continue;
             if (dp[i][j] == -1) continue;
             if (dp[i-1][j] == -1 || dp[i][j-1] == -1)
                 dp[i][j] = (dp[i-1][j] + dp[i][j-1]+1) % 1000000007;
