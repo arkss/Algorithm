9ab6fa0793dcc029551fbcaa5da300b80c812d2f
minsung <rkdalstjd9@naver.com>
2020-04-20 14:19:59 +0900

    N과 M(6)

---
+++ BAEKJOON/15655/15655.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> arr;
+vector<int> answer;
+bool visited[9];
+
+void backtracking(int N, int M, int cnt){
+    if (M == cnt){
+        for (int i=0;i<M;i++){
+            cout << answer[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i=0;i<N;i++){
+        if (!visited[i]){
+            if (!answer.empty()){
+                if (answer[answer.size()-1] > arr[i]) continue;
+            }
+            visited[i] = 1;
+            answer.push_back(arr[i]);
+            backtracking(N, M, cnt+1);
+            answer.pop_back();
+            visited[i] = 0;
+        }
+    }
+}
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+
+    for (int i=0;i<N;i++){
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    backtracking(N, M, 0);
+    return 0;
+}
