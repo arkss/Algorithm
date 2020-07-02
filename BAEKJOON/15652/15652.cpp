124feca364bc13b3c55ae2db7b9435f83920c68c
minsung <rkdalstjd9@naver.com>
2020-07-02 22:23:01 +0900

    15652 컨벤션 적용

--- BAEKJOON/15652/15652.cpp
+++ BAEKJOON/15652/15652.cpp
@@ -5,26 +5,33 @@ using namespace std;
 
 vector<int> answer;
 
-void backtracking(int N, int M, int cnt){
-    if (M == cnt){
-        for (int i=0;i<answer.size();i++){
+void backtracking(int N, int M, int cnt)
+{
+    if (M == cnt)
+    {
+        for (int i = 0; i < answer.size(); i++)
+        {
             cout << answer[i] << " ";
         }
         cout << "\n";
         return;
     }
 
-    for (int i=1;i<=N;i++){
-        if (!answer.empty()){
-            if (answer[answer.size()-1] > i) continue;
+    for (int i = 1; i <= N; i++)
+    {
+        if (!answer.empty())
+        {
+            if (answer[answer.size() - 1] > i)
+                continue;
         }
         answer.push_back(i);
-        backtracking(N, M, cnt+1);
+        backtracking(N, M, cnt + 1);
         answer.pop_back();
     }
 }
 
-int main(){
+int main()
+{
     int N, M;
     cin >> N >> M;
 
