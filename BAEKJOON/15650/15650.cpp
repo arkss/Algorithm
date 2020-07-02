3307dfe843bd0a0168c5b0756b86f92fc3a257f5
minsung <rkdalstjd9@naver.com>
2020-07-02 18:59:08 +0900

    15650 컨벤션 적용

--- BAEKJOON/15650/15650.cpp
+++ BAEKJOON/15650/15650.cpp
@@ -6,36 +6,44 @@ using namespace std;
 bool visited[9];
 vector<int> answer;
 
-void backtracking(int N,int M, int cnt){
-    if (M == cnt){
-        for (int i=0;i<answer.size();i++){
+void backtracking(int N, int M, int cnt)
+{
+    if (M == cnt)
+    {
+        for (int i = 0; i < answer.size(); i++)
+        {
             cout << answer[i] << " ";
         }
         cout << "\n";
         return;
     }
-    else {
-        for (int i=1;i<=N;i++){
-            
-            if (!visited[i]){
-                if (!answer.empty()){
-                    if (answer[answer.size()-1] > i) continue;
+    else
+    {
+        for (int i = 1; i <= N; i++)
+        {
+
+            if (!visited[i])
+            {
+                if (!answer.empty())
+                {
+                    if (answer[answer.size() - 1] > i)
+                        continue;
                 }
                 visited[i] = 1;
                 answer.push_back(i);
-                backtracking(N,M,cnt+1);
+                backtracking(N, M, cnt + 1);
                 answer.pop_back();
                 visited[i] = 0;
             }
         }
     }
-
 }
 
-int main(){
-    int N,M;
+int main()
+{
+    int N, M;
     cin >> N >> M;
 
-    backtracking(N,M,0);
+    backtracking(N, M, 0);
     return 0;
 }
