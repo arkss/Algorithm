51abd2effffe86dff7df70b26751a44620ceeb80
minsung <rkdalstjd9@naver.com>
2020-07-02 22:49:13 +0900

    15654 컨벤션 적용

--- BAEKJOON/15654/15654.cpp
+++ BAEKJOON/15654/15654.cpp
@@ -8,32 +8,39 @@ vector<int> arr;
 vector<int> answer;
 bool visited[9];
 
-void backtracking(int N, int M, int cnt){
-    if (M == cnt){
-        for (int i=0;i<answer.size();i++){
+void backtracking(int N, int M, int cnt)
+{
+    if (M == cnt)
+    {
+        for (int i = 0; i < answer.size(); i++)
+        {
             cout << answer[i] << " ";
         }
         cout << "\n";
         return;
     }
 
-    for (int i=0;i<arr.size();i++){
-        if (!visited[i]){
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (!visited[i])
+        {
             visited[i] = 1;
             answer.push_back(arr[i]);
-            backtracking(N, M, cnt+1);
+            backtracking(N, M, cnt + 1);
             answer.pop_back();
             visited[i] = 0;
         }
     }
 }
 
-int main(){
-    int N,M;
+int main()
+{
+    int N, M;
 
     cin >> N >> M;
 
-    for (int i=0;i<N;i++){
+    for (int i = 0; i < N; i++)
+    {
         int temp;
         cin >> temp;
         arr.push_back(temp);
