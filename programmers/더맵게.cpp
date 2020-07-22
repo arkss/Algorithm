09bc338ca7aed987412f10a2352ea34cdcc4ebbc
minsung <rkdalstjd9@naver.com>
2020-07-23 02:35:33 +0900

    더 맵게 알고리즘 스터디 준비

--- programmers/더맵게.cpp
+++ programmers/더맵게.cpp
@@ -1,37 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
 
 using namespace std;
 
-int solution(vector<int> scoville, int x){
+int solution(vector<int> scovile, int x)
+{
     int answer = 0;
-    
-    priority_queue<int, vector<int>, greater<int> > pq (scoville.begin(), scoville.end());
-
-    while(1){
-        //cout << pq.top() << "\n";
-        if (pq.top() >= x) break;
-        else  if (pq.size() == 1) return -1;
+    priority_queue<int, vector<int>, greater<int>> pq(scovile.begin(), scovile.end());
+    while (1)
+    {
+        if (pq.top() >= x)
+            break;
+        if (pq.size() == 1)
+            return -1;
         int first = pq.top();
         pq.pop();
         int second = pq.top();
         pq.pop();
-        pq.push(first+second*2);
+        pq.push(first + second * 2);
         answer++;
     }
     return answer;
 }
 
-int main(){
-    int k = 7;
-    vector<int> scoville;
-    scoville.push_back(1);
-    scoville.push_back(2);
-    scoville.push_back(3);
-    scoville.push_back(9);
-    scoville.push_back(10);
-    scoville.push_back(12);
-    cout << solution(scoville, k) << "\n";
+int main()
+{
+    vector<int> v = {1, 2, 3, 9, 10, 12};
+    int K = 7;
+    cout << solution(v, K) << "\n";
 }
