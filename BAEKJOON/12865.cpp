c0afefce41555c84702bef5388f4dee645bdeb30
minsung <rkdalstjd9@naver.com>
2020-08-21 00:41:30 +0900

    평범한 배낭

--- BAEKJOON/12865.cpp
+++ BAEKJOON/12865.cpp
@@ -1,3 +1,59 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// int dp[100001];
+
+// bool cmp(const pair<int, int> &a, const pair<int, int> &b)
+// {
+//     return a.first < b.first;
+// }
+
+// void print_dp(int K)
+// {
+//     for (int i = 1; i <= K; i++)
+//     {
+//         cout << dp[i] << " ";
+//     }
+//     cout << "\n";
+// }
+
+// int max3(int a, int b, int c)
+// {
+//     return max(max(a, b), c);
+// }
+
+// int main()
+// {
+//     int N, K;
+//     cin >> N >> K;
+
+//     vector<pair<int, int>> v;
+
+//     for (int i = 0; i < N; i++)
+//     {
+//         int W, V;
+//         cin >> W >> V;
+//         v.push_back(make_pair(W, V));
+//     }
+
+//     sort(v.begin(), v.end(), cmp);
+//     for (int j = 0; j < N; j++)
+//     {
+//         for (int i = K; i >= 1; i--)
+//         {
+//             int W = v[j].first;
+//             int V = v[j].second;
+//             if (i >= W)
+//                 dp[i] = max(dp[i], dp[i - W] + V);
+//         }
+//     }
+
+//     cout << dp[K] << "\n";
+// }
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,11 +61,6 @@
 using namespace std;
 
 int dp[100001];
-
-bool cmp(const pair<int, int> &a, const pair<int, int> &b)
-{
-    return a.first < b.first;
-}
 
 void print_dp(int K)
 {
@@ -20,36 +71,30 @@ void print_dp(int K)
     cout << "\n";
 }
 
-int max3(int a, int b, int c)
-{
-    return max(max(a, b), c);
-}
-
 int main()
 {
     int N, K;
     cin >> N >> K;
 
-    vector<pair<int, int>> v;
+    vector<pair<int, int>> products;
 
     for (int i = 0; i < N; i++)
     {
-        int W, V;
-        cin >> W >> V;
-        v.push_back(make_pair(W, V));
+        int w, v;
+        cin >> w >> v;
+        products.push_back({w, v});
     }
 
-    sort(v.begin(), v.end(), cmp);
-    for (int j = 0; j < N; j++)
+    sort(products.begin(), products.end());
+    for (int i = 0; i < products.size(); i++)
     {
-        for (int i = K; i >= 1; i--)
+        int w = products[i].first;
+        int v = products[i].second;
+        for (int j = K; j >= 1; j--)
         {
-            int W = v[j].first;
-            int V = v[j].second;
-            if (i >= W)
-                dp[i] = max(dp[i], dp[i - W] + V);
+            if (j >= w)
+                dp[j] = max(dp[j], dp[j - w] + v);
         }
     }
-
     cout << dp[K] << "\n";
 }
