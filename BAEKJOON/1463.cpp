afb47cdadf86c706263f027d4dc9bcbe2c32c1a3
minsung <rkdalstjd9@naver.com>
2020-08-25 16:38:37 +0900

    1로 만들기

--- BAEKJOON/1463.cpp
+++ BAEKJOON/1463.cpp
@@ -1,5 +1,31 @@
+// #include <iostream>
+// #include <algorithm>
+
+// using namespace std;
+
+// int dp[1000001];
+
+// int main()
+// {
+//     int N;
+//     cin >> N;
+
+//     dp[1] = 0;
+
+//     for (int i = 2; i <= N; i++)
+//     {
+//         dp[i] = dp[i - 1];
+//         if (i % 3 == 0)
+//             dp[i] = min(dp[i], dp[i / 3]);
+//         if (i % 2 == 0)
+//             dp[i] = min(dp[i], dp[i / 2]);
+//         dp[i]++;
+//     }
+
+//     cout << dp[N] << "\n";
+// }
+
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
@@ -9,18 +35,19 @@ int main()
 {
     int N;
     cin >> N;
-
     dp[1] = 0;
 
     for (int i = 2; i <= N; i++)
     {
-        dp[i] = dp[i - 1];
+        dp[i] = dp[i - 1] + 1;
         if (i % 3 == 0)
-            dp[i] = min(dp[i], dp[i / 3]);
+            dp[i] = min(dp[i], dp[i / 3] + 1);
         if (i % 2 == 0)
-            dp[i] = min(dp[i], dp[i / 2]);
-        dp[i]++;
+            dp[i] = min(dp[i], dp[i / 2] + 1);
     }
+
+    // for (int i = 1; i <= N; i++)
+    //     cout << dp[i] << " ";
 
     cout << dp[N] << "\n";
 }
