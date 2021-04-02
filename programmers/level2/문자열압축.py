6d0e7d5fd89881c347477f896632269142ce615c
minsung <rkdalstjd9@naver.com>
2021-04-03 03:10:56 +0900

    문자열압축

---
+++ programmers/level2/문자열압축.py
@@ -0,0 +1,32 @@
+def solution(s):
+    if len(s) == 1:
+        return 1
+    min_length = 987654321
+
+    for i in range(1, len(s)//2+1):
+        cnt = 1
+        now_len = 0
+        prev = s[0:i]
+
+        for idx in range(i, len(s), i):
+            now = s[idx:idx+i]
+
+            if now == prev:
+                cnt += 1
+
+            else:
+                now_len += len(prev)
+                if cnt != 1:
+                    now_len += len(str(cnt))
+                cnt = 1
+            prev = now
+        now_len += len(prev)
+        if cnt != 1:
+            now_len += len(str(cnt))
+
+        min_length = min(min_length, now_len)
+
+    return min_length
+
+
+# aabbaccc
