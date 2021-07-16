474448d882ec2e85bb4564c89bb91d299124b609
minsung <rkdalstjd9@naver.com>
2021-07-16 23:59:11 +0900

    창고 다각형

--- BAEKJOON/2304.py
+++ BAEKJOON/2304.py
@@ -2,35 +2,60 @@ import sys
 
 input = sys.stdin.readline
 
+
+def find_idx_of_max_h(data):
+    max_h = 0
+    max_idx = 0
+
+    for idx, pos in enumerate(data):
+        x, h = pos
+        if h > max_h:
+            max_h = h
+            max_idx = idx
+
+    return max_idx
+
+
 if __name__ == "__main__":
     N = int(input())
 
-    position = []
+    data = []
 
     for _ in range(N):
-        x, y = map(int, input().split())
-        position.append((x, y))
+        x, h = map(int, input().split())
 
-    position = sorted(position)
+        data.append((x, h))
 
-    stack = []
+    data = sorted(data)
 
-    area = 0
+    # print(data)
 
-    for x, h in position:
-        print(f"({x}, {h})")
-        if stack and stack[-1][1] > h:
-            continue
-        elif stack and stack[-1][1] < h:
-            print(f"{stack[-1][1]} {h}")
-            prev_x, prev_h = stack.pop()
-            area += prev_h * (x-prev_x)
+    max_h_idx = find_idx_of_max_h(data)
 
-            print(f"더해지는 수 : {prev_h * (x-prev_x)}")
-            print(f"넓이 : {area}")
+    # print(data[:max_h_idx])
 
-            stack.append((x, h))
-        else:
-            stack.append((x, h))
-    print(stack)
-    print(area)
+    total_size = data[max_h_idx][1]
+
+    # 왼쪽 계산
+    max_x, max_h = data[:max_h_idx+1][0]
+    for x, h in data[:max_h_idx+1]:
+        if h > max_h:
+            add_size = max_h * (x - max_x)
+            total_size += add_size
+
+            max_x = x
+            max_h = h
+
+    # 오른쪽 계산
+    right_data = list(reversed(data[max_h_idx:]))
+    max_x, max_h = right_data[0]
+
+    for x, h in data[:max_h_idx+1]:
+        if h > max_h:
+            add_size = max_h * (max_x - x)
+            total_size += add_size
+
+            max_x = x
+            max_h = h
+
+    print(total_size)
